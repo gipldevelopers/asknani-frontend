c1c72a900b85f51133841857c349cd675b9839a2
Ayush Solanki <103575160+ayushsolanki29@users.noreply.github.com>
2025-09-26 18:17:51 +0530

    frontend push

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "images.unsplash.com",
-            }, {
-                protocol: "https",
-                hostname: "placehold.co",
-            }, {
-                protocol: "https",
-                hostname: "plus.unsplash.com",
-            },
-        ],
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "placehold.co",
+      },
+      {
+        protocol: "https",
+        hostname: "plus.unsplash.com",
+      },
+    ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
